d55c190d01d2058f7aee983899023cd57314d15d
Jake Nylund <jakegny@gmail.com>
2017-08-12 20:33:58 -0700

    First commit

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+	public Sprite dmgSprite;
+	public int hp = 4;
+    public AudioClip chopSounds1;
+    public AudioClip chopSounds2;
+
+	private SpriteRenderer spriteRenderer;
+
+	void Awake () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	public void DamageWall (int loss) {
+        SoundManager.instance.RandomizeSfx(chopSounds1, chopSounds2);
+		spriteRenderer.sprite = dmgSprite;
+		hp -= loss;
+		if (hp <= 0) {
+			gameObject.SetActive(false);
+		}
+	}
+}
